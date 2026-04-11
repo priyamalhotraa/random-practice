35a8506668af3d20e4c137d5e718d65ab5299124
priya <priyamalhotra14012005@gmail.com>
2026-04-12 01:47:55 +0530

    u

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
 import React from 'react'
 
-function App() {
+function app() {
   return (
-    <div className='flex min-h-screen bg-[linear-gradient(180deg,#f6fbf8,#f8fbff_50%,#ffffff)]'>
-      <div className=' w-full bg-zinc-900 text-white font-sans text-6xl text-justify my-6' > helllllo</div>
-      <div className=' w-full bg-blue-300 text-black font-serif text-5xl text-left tracking-widest py-9'> yooo</div>
-      <div className=' w-full bg-zinc-900 text-white font-mono text-4xl mx-6 px-8'> heelo</div>
-    </div>
+    <div>app</div>
   )
 }
 
-export default App
+export default app
