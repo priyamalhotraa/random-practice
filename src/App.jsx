b83b1b9dae9c010f200a6feb3a01fb0ead49e8ef
priya <priyamalhotra14012005@gmail.com>
2026-04-12 02:48:08 +0530

    trying new

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,17 @@ function app() {
   return (
     <div className='h-screen w-screen bg-black flex'>
       <div className='h-full w-6/12 bg-white'>
-      <h1>The expert in <span className='text-[#ad8b48]'>Anything 
-        <br/>
+      <h1 className='font-semibold text-6xl mx-20 my-54'>The expert in <span className='text-[#ad8b48]'>Anything 
         was once</span> a
         <br/> BEGINNER.
       </h1>
+      <button className='bg-[#ad8b48] p-2 '>Let's Rock.</button>
       </div>
       <div className='h-full w-6/12 bg-red-200'>
-      <img className='h-full w-full object-cover' src = "https://i.pinimg.com/236x/ee/0e/17/ee0e17eea5afa9decc7557bf56a650a5.jpg"></img>
+      <img className='h-full w-full object-cover ' src = "https://i.pinimg.com/736x/5a/22/ac/5a22aca8ef3f27a29a2083c99ff9cd94.jpg"></img>
       </div>
     </div>
   )
 }
 
-export default app
+export default app 
